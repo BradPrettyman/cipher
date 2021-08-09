e1d749b243a97c79e3f0dba46fbf2e0f94384a1e
Brad Prettyman <brad.prettyman5@gmail.com>
2021-08-09 16:30:35 -0700

    more functions for cipher Co-authored-by: Benjamin Wilson <benjaminw1030@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-});
+// Business Logic
 let secret = prompt("Tell us a secret:")
 
 function capitalizeLetters(string) {
@@ -10,26 +9,29 @@ function reverseLetters(string) {
   return (string.charAt(string.length - 1) + string.charAt(0)).toUpperCase();
 }
 
-function cipher(string) {
-  console.log(string);
-  console.log(capitalizeLetters(string));
-  console.log(secret.concat(reverseLetters(string)));
-  console.log(lastLetter(string).concat(secret.concat(reverseLetters(string))));
+
+function reverse(string) {
+  return string.split("").reverse().join("");
 }
 
 function lastLetter(string) {
   return (string.charAt(string.length / 2));
 }
-// .concat(cipher(string))
-cipher(secret);
-// cipher2(secret);
 
-// function capitalizeLastLetter(string) {  
-//   return string.charAt(string.length-1).toUpperCase();
-// }
+function cipher(string) {
+  console.log(string);
+  console.log(capitalizeLetters(string));
+  console.log(secret.concat(reverseLetters(string)));
+  console.log(lastLetter(string).concat(secret.concat(reverseLetters(string))));
+  console.log(reverse(lastLetter(string).concat(secret.concat(reverseLetters(string)))));
+}
 
-// let secret = prompt("Tell us a secret:")
-// console.log(secret);
-// console.log(capitalizeLetters(secret));
-// console.log(reverseLetters(secret));
-// console.log(capitalizeLastLetter(secret));
+// cipher(secret);
+
+// UI Logic
+$(document).ready(function() {
+  
+  cipher(secret);
+
+  $(.original).click(function())
+});
