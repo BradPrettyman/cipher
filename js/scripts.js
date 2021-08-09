dadb9924a3ab402831937473bd0fd4771b56bc13
Brad Prettyman <brad.prettyman5@gmail.com>
2021-08-09 15:41:48 -0700

    add function for secret code Co-authored-by: Benjamin Wilson <benjaminw1030@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
 });
+let secret = prompt("Tell us a secret:")
+
+function capitalizeLetters(string) {
+  return (string.charAt(0) + string.charAt(string.length - 1)).toUpperCase();
+}
+
+function reverseLetters(string) {
+  return (string.charAt(string.length - 1) + string.charAt(0)).toUpperCase();
+}
+
+function cipher(string) {
+  console.log(string);
+  console.log(capitalizeLetters(string));
+  console.log(secret.concat(reverseLetters(string)));
+  console.log(lastLetter(string).concat(secret.concat(reverseLetters(string))));
+}
+
+function lastLetter(string) {
+  return (string.charAt(string.length / 2));
+}
+// .concat(cipher(string))
+cipher(secret);
+// cipher2(secret);
+
+// function capitalizeLastLetter(string) {  
+//   return string.charAt(string.length-1).toUpperCase();
+// }
+
+// let secret = prompt("Tell us a secret:")
+// console.log(secret);
+// console.log(capitalizeLetters(secret));
+// console.log(reverseLetters(secret));
+// console.log(capitalizeLastLetter(secret));
